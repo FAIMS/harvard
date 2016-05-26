2d26960315f17a3fa3ce0cb4c87c36913e9287e4
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-05-27 02:36:24 +1000

    I should really start providing better commit messages...

--- proc/post.sh
+++ proc/post.sh
@@ -76,5 +76,54 @@ replacement="\\1
   setContextDateOpened();"
 perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_logic.bsh
 
+string="
+        <select1 ref=\"Entity_Types\">
+          <label>{Entity_Types}<\/label>
+          <item>
+            <label>Options not loaded<\/label>
+            <value>Options not loaded<\/value>
+          <\/item>
+        <\/select1>"
+replacement="
+        <group ref=\"Colgroup_1\" faims_style=\"orientation\">
+          <label\/>
+          <group ref=\"Col_0\" faims_style=\"even\">
+            <label\/>
+            <select1 ref=\"Entity_Types\">
+              <label>{Entity_Types}<\/label>
+              <item>
+                <label>Options not loaded<\/label>
+                <value>Options not loaded<\/value>
+              <\/item>
+            <\/select1>
+          <\/group>
+          <group ref=\"Col_1\" faims_style=\"even\">
+            <label\/>
+            <select1 ref=\"Select_User\">
+              <label>{Select_User}<\/label>
+              <item>
+                <label>Options not loaded<\/label>
+                <value>Options not loaded<\/value>
+              <\/item>
+            <\/select1>
+          <\/group>
+        <\/group>"
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+string="
+              <Entity_Types\/>"
+replacement="
+              <Colgroup_1>
+                <Col_0>
+                  <Entity_Types\/>
+                <\/Col_0>
+                <Col_1>
+                  <Select_User\/>
+                <\/Col_1>
+              <\/Colgroup_1>
+              <Select_User\/>"
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+echo "Select_User=Select User" >> english.0.properties
+
 rm ui_logic.bsh.original
 rm ui_schema.xml.original
+
