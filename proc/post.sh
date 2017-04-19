b9830adf182c26175c69b57b3e43a866a2a62fd0
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2017-04-19 10:13:14 +1000

    Cleaned up exporter output

--- proc/post.sh
+++ proc/post.sh
@@ -7,32 +7,6 @@ cd ..
 
 cd module
 
-# Notify the current user if they try to add relationships to the current
-# context without having saved it first
-string="
-  newTab(\"Context_Group_Relationship\", true);"
-replacement="
-  String tabgroup = \"Context_Group\";
-  if (isNull(getUuid(tabgroup))){
-    showToast(\"{You_must_save_this_tabgroup_first}\");
-    return;
-  }
-  $string"
-perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
-
-# Notify the current user if they try to add relationships to the current
-# context without having saved it first
-string="
-  newTab(\"Relationship\", true);"
-replacement="
-  String tabgroup = \"Context\";
-  if (isNull(getUuid(tabgroup))){
-    showToast(\"{You_must_save_this_tabgroup_first}\");
-    return;
-  }
-  $string"
-perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
-
 string="
         <select1 ref=\"Entity_Types\">
           <label>{Entity_Types}<\/label>
@@ -112,9 +86,7 @@ perl -0777 -i.original -pe "s/$string/$replacement/igs" validation.xml
 echo "Select_User=Select User"                                     >> english.0.properties
 echo "Select_Site=Select Site"                                     >> english.0.properties
 echo "Volume_Liters=Volume (Liters)"                               >> english.0.properties
-echo "All_Users=All Users"                                         >> english.0.properties
 echo "Must_be_between_0_0_and_90_0=(Must be between 0.0 and 90.0)" >> english.0.properties
 
-rm ui_logic.bsh.original
 rm ui_schema.xml.original
 rm validation.xml.original
